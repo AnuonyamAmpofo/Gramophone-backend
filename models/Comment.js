80dbaa5dfcaf732c0d374cdf7b8e0028bc143f1a
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2024-09-18 19:46:15 +0000

    comment2

--- models/Comment.js
+++ models/Comment.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
     courseCode: { type: String, required: true },
     studentID: { type: String, required: true },
+    studentName:{type: String},
     comment: { type: String, required: true },
     instructorID: { type: String, required: true },
+    instructorName: {type: String},
+
     datePosted: { type: Date, default: Date.now },
 });
 
