9df5299f59152ef6cc787dfc8a8bf69ff36d9bce
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2025-04-14 11:04:29 +0000

    .

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -313,7 +313,7 @@ const AdminController = {
             return res.status(404).json({ message: 'Instructor not found' });
         }
 
-        const deletdCourse = await Course.deleteMany({ instructorID });
+        const deletdCourses = await Course.deleteMany({ instructorID });
 
         // Step 2: Find all courses referencing the student in their sessions
         // const coursesWithInstructor = await Course.find({ "instructorID": instructorID });
@@ -328,6 +328,7 @@ const AdminController = {
         res.status(200).json({
             message: 'Instructor and associated courses deleted successfully',
             deletedInstructor,
+            deletedCourses: deletedCourses.deletedCount, // Number of courses deleted
             // updatedCourses: updateCourses.modifiedCount,
             coursesWithInstructor
         });
